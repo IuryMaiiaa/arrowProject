6e381eaf4562c93d0368482547de23e70a2e8955
Neils01 <iurryy@yahoo.com.br>
2015-08-07 21:00:02 -0300

    att interface

--- Assets/Scripts/GerenciadorJogo.cs
+++ Assets/Scripts/GerenciadorJogo.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class GerenciadorJogo : MonoBehaviour {
 	public GerenciadorFase faseManager;
+	public bool existeboss;
+	public GameObject BossDragao;
+	public GameObject BossInstanciado;
+	public bool bossInvocado;
+
 
 	// Use this for initialization
 	void Start () {
+		BossInstanciado = null;
+		existeboss = false;
 		faseManager = (GerenciadorFase)FindObjectOfType(typeof(GerenciadorFase));
+		bossInvocado = false;
 	}
 	
 	// Update is called once per frame
@@ -15,6 +23,22 @@ public class GerenciadorJogo : MonoBehaviour {
 
 	public void AtualizarGame() {
 		faseManager.rotacionarFases();
+		if (!existeboss) {
+			if (7 < (int)(Random.value * 100) % 10) {
+				emitirAlerta ();
+				existeboss = true;
+			}
+		} else if (!bossInvocado) {
+			instanciarBoss(Random.Range(1,1));
+			bossInvocado=true;
+		}
+	}
+
+	public void instanciarBoss (int valorBoss) {
+		if (valorBoss == 1) {
+			BossInstanciado = GameObject.Instantiate(BossDragao) as GameObject;
+		}
+
 	}
 
 	public void sendDefault() {
@@ -23,5 +47,14 @@ public class GerenciadorJogo : MonoBehaviour {
 
 	public void reiniciar() {
 		Application.LoadLevel(0);
+	}
+
+	public void emitirAlerta() {
+		Debug.Log("WARNING WARNING WARNING WARNING");
+	}
+
+	public void bossMorto() {
+		existeboss = false;
+		bossInvocado = false;
 	}
 }
